6da2636cebecbdaf509021817f36dc70e2ff3bc2
Blake Akapita <bakapita10@gmail.com>
2019-07-24 17:16:35 +1200

    Wrote constants for damage codes and created a method that retrieves the maintenance fault

--- RATA_FMM/RATA_FMM/Form1.cs
+++ RATA_FMM/RATA_FMM/Form1.cs
@@ -10,11 +10,37 @@ using System.Windows.Forms;
 
 namespace RATA_FMM
 {
+    
+
     public partial class Form1 : Form
     {
+
+        static string[] MAINTENANCE_CODES = { "a" , "b" , "c" , "d" , "e" , "f" };
+        static string[] MAINTENANCE_FAULTS = { "Trip Hazard" , "Vertical Displacement" , "Horizontal Displacement" , "Broken" , "Hole" , "Poor Previous Reinstatement"};
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Takes the maintenance code and returns the maintenance fault as a string
+        /// </summary>
+        /// <param name="c">The maintenance code</param>
+        /// <returns>The maintenance fault</returns>
+        public string getMaintenanceFault (string c)
+        {
+            string fault = "";
+
+            for (int i = 0; i < MAINTENANCE_CODES.Length; i++)
+            {
+                if (c == MAINTENANCE_CODES[i]) //If the code being searched on matches the code at the current position
+                {
+                    fault = MAINTENANCE_FAULTS[i]; //Assign it the appropriate maintenance fault
+                }
+            }
+
+            return fault; //Return the fault
         }
     }
 }
