a906b326d68d25a62c5dffc74642dab6bbc4d430
Blake Akapita <bakapita10@gmail.com>
2019-07-25 08:35:52 +1200

    Added method to rank footpaths based on condition from 1-5 from worst to best (5 being worst and 1 being best)

--- RATA_FMM/RATA_FMM/Form1.cs
+++ RATA_FMM/RATA_FMM/Form1.cs
@@ -21,6 +21,7 @@ namespace RATA_FMM
         public Form1()
         {
             InitializeComponent();
+            rankOnSeverity();
         }
 
         /// <summary>
@@ -41,6 +42,49 @@ namespace RATA_FMM
             }
 
             return fault; //Return the fault
+        }
+
+        /// <summary>
+        /// Ranks data entries based on severity, with 5 being prioritized first and 1 last
+        /// Very crude ranking implementation at present
+        /// </summary>
+        public void rankOnSeverity ()
+        {
+            List<string[]> footpaths = new List<string[]>(); //A list to hold all footpaths
+
+            //Some temporary dummy entries
+            string[] dummyEntryOne = {"Albert Street" , "5"};
+            string[] dummyEntryTwo = { "Anzac Street" , "4"};
+            string[] dummyEntryThree = { "Hurley Place" , "6"};
+            string[] dummyEntryFour = { "Queen Street" , "2"};
+            string[] dummyEntryFive = { "Shakespeare Street" , "1"};
+            string[] dummyEntrySix = { "Taylor Street" , "1"};
+            string[] dummyEntrySeven = { "Thornton Road" , "2"};
+            string[] dummyEntryEight = { "Victoria Street" , "3"};
+            string[] dummyEntryNine = { "Cotter Place" , "4"};
+            string[] dummyEntryTen = { "Wallace Terrace" , "5"};
+
+            //Adding dummy entries to footpath list
+            footpaths.Add(dummyEntryOne);
+            footpaths.Add(dummyEntryTwo);
+            footpaths.Add(dummyEntryThree);
+            footpaths.Add(dummyEntryFour);
+            footpaths.Add(dummyEntryFive);
+            footpaths.Add(dummyEntrySix);
+            footpaths.Add(dummyEntrySeven);
+            footpaths.Add(dummyEntryEight);
+            footpaths.Add(dummyEntryNine);
+            footpaths.Add(dummyEntryTen);
+
+            footpaths = footpaths.OrderByDescending(arr => arr[1]).ToList(); //Sort the footpath list from highest severity to lowest
+
+            //Output the footpath report to the console window
+            Console.WriteLine("Footpath Severity Report:");
+            for (int i = 0; i < footpaths.Count; i++)
+            {
+                string[] currFootpath = footpaths[i];
+                Console.WriteLine(currFootpath[0] + " with severity " + currFootpath[1]); //Output street name and severity
+            }
         }
     }
 }
