b4ccec39b9d2c81e56b7a8b153a96b22f524639d
arcguy <arcguy302@gmail.com>
2019-08-05 13:25:56 +1200

    Merged blakes changes to allow printing of report

--- RATA_FMM/RATA_FMM/Form1.cs
+++ RATA_FMM/RATA_FMM/Form1.cs
@@ -15,6 +15,10 @@ namespace RATA_FMM
     {
         const string FILTER = "Excel Files (*.xlsx)|*.xlsx|All Files (*.*)|*.*";
         List<Road> roadList = new List<Road>();
+
+        static string[] MAINTENANCE_CODES = { "a", "b", "c", "d", "e", "f" };
+        static string[] MAINTENANCE_FAULTS = { "Trip Hazard", "Vertical Displacement", "Horizontal Displacement", "Broken", "Hole", "Poor Previous Reinstatement" };
+
         int window_length = Screen.PrimaryScreen.Bounds.Width;
         int window_height = Screen.PrimaryScreen.Bounds.Height;
 
@@ -24,7 +28,7 @@ namespace RATA_FMM
             //setting sizes and positions of listboxes
             listBoxData.Width = window_length / 3 - 50;
             listBoxData.Height = window_height - 100;
-            listBoxReplacement.Location = new System.Drawing.Point(10, 30);
+            listBoxData.Location = new System.Drawing.Point(10, 30);
 
             listBoxReplacement.Height = window_height / 2 - 60;
             listBoxReplacement.Width = window_length / 3 - 50;
@@ -117,12 +121,97 @@ namespace RATA_FMM
             }
         }
 
-        private void DisplayData()
+        /*private void DisplayData()
         {
             //displaying in first listbox
             foreach (Road r in roadList)
             {
                 listBoxData.Items.Add(r.ToString());
+            }
+        }*/
+
+        private void DisplayData()
+        {
+            string text = "";
+
+
+
+            //displaying in first listbox
+            foreach (Road r in roadList)
+            {
+                listBoxData.Items.Add(r.ToString());
+            }
+
+            text += roadList.ElementAt(1);
+            text += "\n";
+            text += roadList.ElementAt(2);
+            text += "\n";
+
+            PCPrint printer = new PCPrint(text);
+            printer.PrinterFont = new System.Drawing.Font("Times New Roman", 14);
+            printer.Print();
+        }
+
+        /// <summary>
+        /// Takes the maintenance code and returns the maintenance fault as a string
+        /// </summary>
+        /// <param name="c">The maintenance code</param>
+        /// <returns>The maintenance fault</returns>
+        private string getMaintenanceFault(string c)
+        {
+            string fault = "";
+
+            for (int i = 0; i < MAINTENANCE_CODES.Length; i++)
+            {
+                if (c == MAINTENANCE_CODES[i]) //If the code being searched on matches the code at the current position
+                {
+                    fault = MAINTENANCE_FAULTS[i]; //Assign it the appropriate maintenance fault
+                }
+            }
+
+            return fault; //Return the fault
+        }
+
+        /// <summary>
+        /// Ranks data entries based on severity, with 5 being prioritized first and 1 last
+        /// Very crude ranking implementation at present
+        /// </summary>
+        private void rankOnSeverity()
+        {
+            List<string[]> footpaths = new List<string[]>(); //A list to hold all footpaths
+
+            //Some temporary dummy entries
+            string[] dummyEntryOne = { "Albert Street", "5" };
+            string[] dummyEntryTwo = { "Anzac Street", "4" };
+            string[] dummyEntryThree = { "Hurley Place", "6" };
+            string[] dummyEntryFour = { "Queen Street", "2" };
+            string[] dummyEntryFive = { "Shakespeare Street", "1" };
+            string[] dummyEntrySix = { "Taylor Street", "1" };
+            string[] dummyEntrySeven = { "Thornton Road", "2" };
+            string[] dummyEntryEight = { "Victoria Street", "3" };
+            string[] dummyEntryNine = { "Cotter Place", "4" };
+            string[] dummyEntryTen = { "Wallace Terrace", "5" };
+
+            //Adding dummy entries to footpath list
+            footpaths.Add(dummyEntryOne);
+            footpaths.Add(dummyEntryTwo);
+            footpaths.Add(dummyEntryThree);
+            footpaths.Add(dummyEntryFour);
+            footpaths.Add(dummyEntryFive);
+            footpaths.Add(dummyEntrySix);
+            footpaths.Add(dummyEntrySeven);
+            footpaths.Add(dummyEntryEight);
+            footpaths.Add(dummyEntryNine);
+            footpaths.Add(dummyEntryTen);
+
+            footpaths = footpaths.OrderByDescending(arr => arr[1]).ToList(); //Sort the footpath list from highest severity to lowest
+
+            //Output the footpath report to the console window
+            Console.WriteLine("Footpath Severity Report:");
+            for (int i = 0; i < footpaths.Count; i++)
+            {
+                string[] currFootpath = footpaths[i];
+                Console.WriteLine(currFootpath[0] + " with severity " + currFootpath[1]); //Output street name and severity
             }
         }
     }
